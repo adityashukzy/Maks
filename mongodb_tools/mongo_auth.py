202e062045c2bf9d3ca21c55e7f5b144abfcdc3b
Aditya Shukla <adityashukzy@gmail.com>
2021-05-04 15:32:22 +0530

    Refactor MongoDB files

---
+++ mongodb_tools/mongo_auth.py
@@ -0,0 +1,29 @@
+import os
+import dns
+import bcrypt
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+def verifyLogin(
+	email,
+	password
+	):
+	# Loading mongodb uri from the environment
+	load_dotenv()
+	mongo_string = os.getenv('MONGODB_AUTH_URI')
+	client = MongoClient(mongo_string)
+
+	database = client["User-Credentials"]
+	coll = database["Email-Password-Credentials"]
+
+	creds = coll.find_one({"email": email})
+	hashed_pwd = creds["password"]
+
+	# Returning if provided plain-text password is same as hashed pwd saved in db
+	return bcrypt.checkpw(password.encode('utf8'), hashed_pwd.encode('utf8'))
+
+if __name__ == "__main__":
+	if verifyLogin(email="adityashukzy@gmail.com", password="password"):
+		print("Login authenticated!")
+	else:
+		print("Wrong credentials!")
--- mongodb_tools/mongo_upload.py
+++ mongodb_tools/mongo_upload.py
@@ -1,18 +1,23 @@
+import os
 import dns
 from pymongo import MongoClient
 from aws_tools import aws_upload
+from dotenv import load_dotenv
 from datetime import datetime
 
 def make_violator_entry_in_db(
 	confidence_pred,
-	img_buffer,
-	mongo_string="mongodb+srv://admin-account:admin-password@maks-cluster.noror.mongodb.net/Test-DB?retryWrites=true&w=majority"
+	img_buffer
 	):
 	"""
 	Inserts entry into MongoDB collection of link to image of violator, confidence scores and timestamp.
 
 	Returns: None
 	"""
+
+	## Getting mongo string from .env
+	load_dotenv()
+	mongo_string = os.getenv('MONGODB_AUTH_URI')
 
 	## Initilizing Mongo client
 	client = MongoClient(mongo_string)
@@ -26,13 +31,14 @@ def make_violator_entry_in_db(
 	date_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 	## Pulling DB variables
-	database = client['Test-DB'] # Picking database from our Mongo client
-	coll = database['Test-Collection'] # Picking collection from the database
+	database = client['User-Credentials'] # Picking database from our Mongo client
+	coll = database['violators'] # Picking collection from the database
 
 	record = {
 		"timestamp": date_string,
 		"confidence-of-prediction": confidence_pred,
 		"link-to-image": uploaded_img_link,
+		"email": os.getenv('EMAIL_ADDRESS')
 	}
 
 	coll.insert_one(record)
